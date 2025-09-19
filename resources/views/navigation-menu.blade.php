c69fd1b1671c042c153a1e3c72bdda6d6d8d77f5
will-2207 <cartuji7@gmail.com>
2025-09-18 21:25:06 -0500

    cambios employees

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -18,6 +18,13 @@
                 </div>
             </div>
 
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link href="{{ route('producto.index') }}" :active="request()->routeIs('producto.*')">
+                         {{ __('producto') }}
+                    </x-nav-link>
+                </div>
+
+
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <!-- Teams Dropdown -->
                 @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+//use App\Http\Controllers\Employee\EmployeeController;
+use App\Http\Controllers\Producto\ProductoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,9 +11,23 @@ Route::get('/', function () {
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
+    'verified'])
+    ->resource(name:'producto',controller:productoController::class)
+    ->names(names: 'producto');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
     'verified',
+
+
+
 ])->group(function () {
+    // Ruta del dashboard
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    // Rutas del recurso producto
+    Route::resource('producto', ProductoController::class)->names('producto');
 });
